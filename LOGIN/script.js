0ed19a79963412d91967308c7a89cdb020fe8fd3
Frank bungey <123551060+frankbungey@users.noreply.github.com>
2023-04-05 13:15:53 +0300

    Add files via upload

---
+++ LOGIN/script.js
@@ -0,0 +1,8 @@
+
+const changePage = document.querySelector(".signup")
+changePage.addEventListener("click",function(){
+    location.href="../SIGNUP/index1.html";
+})
+const button = document.querySelector(".btn").addEventListener("click",function(){
+    alert('login successful');
+})
---
+++ SIGNUP/script1.js
@@ -0,0 +1,7 @@
+const changePage = document.querySelector(".login")
+changePage.addEventListener("click",function(){
+    location.href="../LOGIN/index.html";
+})
+const button = document.querySelector(".btn").addEventListener("click",function(){
+    alert('signup successful');
+})
