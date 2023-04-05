22d18fefc963a8e85efed518dcf83942f7782cc0
Frank bungey <123551060+frankbungey@users.noreply.github.com>
2023-04-05 13:24:03 +0300

    Delete script.js

--- LOGIN/script.js
+++
@@ -1,8 +0,0 @@
-
-const changePage = document.querySelector(".signup")
-changePage.addEventListener("click",function(){
-    location.href="../SIGNUP/index1.html";
-})
-const button = document.querySelector(".btn").addEventListener("click",function(){
-    alert('login successful');
-})
