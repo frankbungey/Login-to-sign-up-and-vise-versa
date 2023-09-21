ef40c7b85cef4aef312ba8e804b9f7e1ba299ade
frankbungey <frankbungey@gmail.com>
2023-09-21 13:33:26 +0300

    validated my login form

--- LOGIN/LOGIN/script.js
+++ LOGIN/LOGIN/script.js
@@ -1,8 +1,27 @@
 
 const changePage = document.querySelector(".signup")
-changePage.addEventListener("click",function(){
+changePage.addEventListener("click", function(){
     location.href="../SIGNUP/index1.html";
-})
-const button = document.querySelector(".btn").addEventListener("click",function(){
-    alert('login successful');
+});
+const email = document.querySelector('.email');
+const span = document.querySelector('.span');
+const span1 = document.querySelector('.span1');
+const password = document.querySelector('#password');
+const button = document.querySelector(".btn").addEventListener("click",()=>{
+    
+    if(email.value === ''){
+       span.innerText = "Email can't be Empty";
+       span.style.color = "red";
+       email.style.border ="1px solid red"
+    }
+    else if(password.value===''){
+    span1.innerText= "Password Required";
+    span1.style.color = "red";
+    password.style.border ="1px solid red";
+    }
+    else if(password.value < 8){
+    span1.innerText= "Password must be atleast 8 characters";
+    span1.style.color = "red";
+    password.style.border ="1px solid red";
+    }
 })
--- LOGIN/SIGNUP/script1.js
+++ LOGIN/SIGNUP/script1.js
@@ -2,6 +2,28 @@ const changePage = document.querySelector(".login")
 changePage.addEventListener("click",function(){
     location.href="../LOGIN/index.html";
 })
-const button = document.querySelector(".btn").addEventListener("click",function(){
-    alert('signup successful');
+
+const firstName = document.querySelector('#fName');
+const lastName = document.querySelector('#lName');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const cPassword = document.querySelector('#cPassword');
+const spn = document.querySelector('.spn');
+const name = document.querySelector('.name');
+
+const button = document.querySelector(".btn").addEventListener("click",(e)=>{
+    let message = [];
+    e.preventDefault();
+    if(firstName.value=== '' && lastName.value === ''){
+    message.push("Names cannot be empty");
+    name.innerText = `${message}`;
+    name.style.color = "red";
+    }
+    if(cPassword.value === password.value){
+        spn.innerText = '';
+    }
+    else{
+        spn.style.color = "red";
+        spn.innerText ="Password doesn't match";
+    }
 })
